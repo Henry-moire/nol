f45409c1899a4e3a6b8aee7d02c3fe753509292f
Nyakundi <henrymoire@gmail.com>
2025-09-24 13:17:18 +0300

    Send email bug fixed
    The error that showed up when a user tries to make an account has been removed.

--- Globals/SendMail.php
+++ Globals/SendMail.php
@@ -8,7 +8,7 @@ use PHPMailer\PHPMailer\Exception;
 class SendMail {
     public function Send_Mail($conf, $mailCnt) {
         //Load Composer's autoloader (created by composer, not included with PHPMailer)
-        require 'Plugins/PHPMailer/vendor/autoload.php';
+        require __DIR__ . '/../vendor/autoload.php';
 
     //Create an instance; passing `true` enables exceptions
     $mail = new PHPMailer(true);
@@ -17,12 +17,12 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_OFF;                     //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = $conf['smtp_host'];                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = $conf['smtp_user'];                     //SMTP username
     $mail->Password   = $conf['smtp_pass'];                     //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = $conf['smtp_port'];                     //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                     //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     // Recipients
     $mail->setFrom($mailCnt['mail_from'], $mailCnt['name_from']);
